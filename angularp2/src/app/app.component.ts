18d1f3f8f418b6dc478fddf868a746bff1afed42
Alejandromm1227 <160982660+Alejandromm1227@users.noreply.github.com>
2024-05-15 22:02:17 -0500

    angular

---
+++ angularp2/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavegacionComponent} from "./components/navegacion/navegacion.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+}

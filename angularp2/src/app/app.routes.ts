ccd74652fff5d01a12cca67e7dd52572ebbc9d63
Alejandromm1227 <160982660+Alejandromm1227@users.noreply.github.com>
2024-05-18 10:27:46 -0500

    angular

--- angularp2/src/app/app.routes.ts
+++ angularp2/src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
-import { ContactoComponent } from "./components/contacto/contacto.component";
-import { IniciarSesionComponent } from "./components/iniciar-sesion/iniciar-sesion.component";
-import { InicioComponent } from "./components/inicio/inicio.component";
-import { NosotrosComponent } from "./components/nosotros/nosotros.component";
-import { PrivadoComponent } from "./components/privado/privado.component";
-import { ProductosComponent } from "./components/productos/productos.component";
-import { ServiciosComponent } from "./components/servicios/servicios.component";
-import { RegistrarseComponent } from "./components/registrarse/registrarse.component";
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
 
 
+
 export const routes: Routes = [
-    {path: "Contacto", component: ContactoComponent},
     {path: "Iniciar Sesion", component: IniciarSesionComponent},
     {path: "Inicio", component: InicioComponent},
     {path: "Nosotros", component: NosotrosComponent},
@@ -20,5 +20,6 @@ export const routes: Routes = [
     {path: "Servicios", component: ServiciosComponent},
     {path: "Registrarse", component: RegistrarseComponent},
     {path: "", redirectTo: "Inicio", pathMatch: "full"},
-    {path: "**", component  : NoEncontradoComponent}     
+    {path: "**", component  : NoEncontradoComponent},
+    {path: "Contacto", component: ContactoComponent} 
 ];
